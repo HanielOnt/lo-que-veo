5eb7f181042233a9bed5b99bb8021de47f3d6aac
HanielOnt <haniel@comprarsitioweb.com>
2024-06-06 20:06:22 -0600

    Dejando el slide como componente y agregandolo al home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import { LogoImg } from '../ui/LogoImg'
+import { Slider } from '../ui/Slider'
 export const Home = () => {
 
 
     return (
         <main >
-            <section className='w-full h-[80vh] flex  justify-center items-center sm:h-[90vh] md:mb-6 lg:mb-8'>
-                <div className='w-fit h-fit flex justify-center items-center '>
+            <section className="w-full h-[80vh]  flex justify-center items-center ">
+                <div className='relative z-10 bg-white'>
                     <LogoImg />
-
                 </div>
-            </section>
+                <div className="mt-20  grid grid-cols-3 w-full h-fit inset-0 z-0 px-4 absolute sm:grid-cols-2 ">
+                    <Slider />
+                    <Slider />
+                    <Slider />
+                </div>
 
+            </section>
 
         </main>
     )
--- src/ui/Slider.jsx
+++ src/ui/Slider.jsx
@@ -1,6 +1,5 @@
 import img1 from "../assets/img/1.webp"
 import img2 from "../assets/img/2.webp"
-import { LogoImg } from "./LogoImg"
 
 
 
@@ -9,72 +8,23 @@ export const Slider = () => {
 
     return (
         <>
-            <div className="w-full h-[80vh]  flex justify-center items-center ">
-
-                <div className="mt-20  grid grid-cols-3 w-full h-fit inset-0 z-0 px-4 absolute sm:grid-cols-2 ">
-                    <div /*logos slide container */ className="bg-red  w-full h-[67vh] flex overflow-hidden px-6 ">   {/* TODO que este div superior se convierta en un componente de UI*/}
-                        <div /*logos slide */ className="w-full h-auto whitespace-nowrap flex flex-col-reverse animate-slider " >
-                            <div className="w-full h-auto flex  flex-col items-end">
-                                {/* TODO hacerle caja de luz al darle click a la imagen */}
-                                <img src={img1} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-center">
-                                <img src={img2} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-end">
-                                <img src={img1} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-start">
-                                <img src={img2} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-end">
-                                <img src={img2} className="w-1/2 h-auto my-2 " />
-                            </div>
-                        </div>
+            <div /*logos slide container */ className="bg-red  w-full h-[67vh] flex overflow-hidden px-6 ">
+                <div /*logos slide */ className="w-full h-auto whitespace-nowrap flex flex-col-reverse animate-slider " >
+                    <div className="w-full h-auto flex  flex-col items-end">
+                        <img src={img1} className="w-1/2 h-auto my-2 " />
                     </div>
-                    <div /*logos slide container */ className="bg-red  w-full h-[67vh] flex overflow-hidden px-6 ">   {/* TODO que este div superior se convierta en un componente de UI*/}
-                        <div /*logos slide */ className="w-full h-auto whitespace-nowrap flex flex-col animate-sliderRev " >
-                            <div className="w-full h-auto flex  flex-col items-end">
-                                {/* TODO hacerle caja de luz al darle click a la imagen */}
-                                <img src={img1} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-center">
-                                <img src={img2} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-end">
-                                <img src={img1} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-start">
-                                <img src={img2} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-end">
-                                <img src={img2} className="w-1/2 h-auto my-2 " />
-                            </div>
-                        </div>
+                    <div className="w-full h-auto flex flex-col items-center">
+                        <img src={img2} className="w-1/2 h-auto my-2 " />
                     </div>
-                    <div /*logos slide container */ className="bg-red  w-full h-[67vh] flex overflow-hidden px-6 ">   {/* TODO que este div superior se convierta en un componente de UI*/}
-                        <div /*logos slide */ className="w-full h-auto whitespace-nowrap flex flex-col-reverse animate-slider " >
-                            <div className="w-full h-auto flex  flex-col items-end">
-                                {/* TODO hacerle caja de luz al darle click a la imagen */}
-                                <img src={img1} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-center">
-                                <img src={img2} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-end">
-                                <img src={img1} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-start">
-                                <img src={img2} className="w-1/2 h-auto my-2 " />
-                            </div>
-                            <div className="w-full h-auto flex flex-col items-end">
-                                <img src={img2} className="w-1/2 h-auto my-2 " />
-                            </div>
-                        </div>
+                    <div className="w-full h-auto flex flex-col items-end">
+                        <img src={img1} className="w-1/2 h-auto my-2 " />
                     </div>
-
-                </div> <div className='relative z-10 '>
-                    <LogoImg />
+                    <div className="w-full h-auto flex flex-col items-start">
+                        <img src={img2} className="w-1/2 h-auto my-2 " />
+                    </div>
+                    <div className="w-full h-auto flex flex-col items-end">
+                        <img src={img2} className="w-1/2 h-auto my-2 " />
+                    </div>
                 </div>
             </div>
         </>
