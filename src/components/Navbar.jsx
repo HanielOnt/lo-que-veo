45bc46ae50d870bde45203d5e079c18e19d35f67
HanielOnt <haniel@comprarsitioweb.com>
2024-06-18 17:10:33 -0600

    Correccion en navbar para hover y palabra

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,13 @@ export const Navbar = () => {
     return (
         <nav >
             <div className="w-full h-fit fixed top-0 flex flex-row justify-around items-center  py-3 bg-white">
-                <NavLink to="https://calendly.com/loqueveo/30min" target="_blank" rel="noopener noreferrer" className="uppercase text-md sm:text-sm lg:text-xl">Agendame</NavLink>
+                <NavLink to="https://calendly.com/loqueveo/30min" target="_blank" rel="noopener noreferrer" className="uppercase text-md sm:text-sm lg:text-xl hover:font-semibold">UN SHOOTING O Q???</NavLink>
                 {
                     location.pathname == '/el-viaje-de-aprender'
                         ? <NavLink to='/home'><LogoImg /></NavLink>
                         : <NavLink to='/el-viaje-de-aprender' className="text-md uppercase text-center  font-openSI leading-none hover:font-openSBI sm:text-sm lg:text-2xl">not famous <br />pero conocido</NavLink>
                 }
-                <NavLink to="https://www.instagram.com/loqueveooo/?hl=en" target="_blank" rel="noopener noreferrer" className="uppercase text-md sm:text-sm lg:text-xl">sígueme</NavLink>
+                <NavLink to="https://www.instagram.com/loqueveooo/?hl=en" target="_blank" rel="noopener noreferrer" className="uppercase text-md sm:text-sm lg:text-xl hover:font-semibold">sígueme</NavLink>
             </div >
         </nav>
     )
